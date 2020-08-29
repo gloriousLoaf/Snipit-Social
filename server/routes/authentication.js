98ae67d1cdaf79f53eee591d17b796222487e559
EricGip <42220408+EricGip@users.noreply.github.com>
2020-08-29 13:41:46 -0700

    connected mongo, fixed dependencies

---
+++ server/routes/authentication.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const passport = require("passport");
+
+// utility functions
+const isLoggedIn = (req, res, next) => {
+    if (req.user) {
+      next();
+    } else {
+      // change unauthorized page here
+      res.sendStatus(401);
+    }
+  };
+
+// api calls
+app.get("/", (req, res) => res.send("hello, please go to /google"));
+
+app.get("/failed", (req, res) => res.send("failure to log in"));
+// just req.user to see the whole json
+app.get("/good", isLoggedIn, (req, res) => res.send(`welcome ${req.user}`));
+
+app.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/failed" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/good");
+  }
+);
+
+app.get("/logout", (req, res) => {
+  // ending the session
+  req.session = null;
+  // passports requires you to do this
+  req.logout();
+  res.redirect("/");
+});
+
+
