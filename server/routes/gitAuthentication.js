3c0ddad68df7c62ef5378ea87c52bbfa2eb42aaa
eric <ericgip97@gmail.com>
2020-09-12 18:19:40 -0700

    fixed routes so only logs git info once

--- server/routes/gitAuthentication.js
+++ server/routes/gitAuthentication.js
@@ -6,38 +6,85 @@ const GitInfo = require("../models/Github");
 
 router.route("/addGitInfo").post((req, res) => {
   // always returns in req.body apparently?
+  GitInfo.findOne(
+    {
+      id: req.body.id
+    },
+    function(err, gitInfo) {
+      if (err) {
+        return console.log(err);
+      }
+
+      if (!gitInfo) {
+        const newInfo = new GitInfo({
+          id: req.body.id,
+
+          name: req.body.name,
+
+          htmlUrl: req.body.htmlURL,
+
+          avatarUrl: req.body.avatarUrl,
+
+          bio: req.body.bio,
+
+          blog: req.body.blog,
+
+          company: req.body.company,
+
+          hireable: req.body.hireable
+        });
+
+        newInfo
+          .save()
+          .then(post => res.json(post))
+          .catch(err => console.log(err));
+      }
+      gitInfo
+    }
+  );
   console.log(req.body);
-  const id = req.body.id;
+  // const id = req.body.id;
 
-  const name = req.body.name;
+  // const name = req.body.name;
 
-  const htmlUrl = req.body.htmlURL;
+  // const htmlUrl = req.body.htmlURL;
 
-  const avatarUrl = req.body.avatarUrl;
+  // const avatarUrl = req.body.avatarUrl;
 
-  const bio = req.body.bio;
+  // const bio = req.body.bio;
 
-  const blog = req.body.blog;
+  // const blog = req.body.blog;
 
-  const company = req.body.company;
+  // const company = req.body.company;
 
-  const hireable = req.body.hireable;
+  // const hireable = req.body.hireable;
 
-  const newInfo = new GitInfo({
-    id,
-    name,
-    htmlUrl,
-    avatarUrl,
-    bio,
-    blog,
-    company,
-    hireable
-  });
+  // const newInfo = new GitInfo({
+  //   id,
+  //   name,
+  //   htmlUrl,
+  //   avatarUrl,
+  //   bio,
+  //   blog,
+  //   company,
+  //   hireable
+  // });
 
-  newInfo
-    .save()
-    .then(post => res.json(post))
-    .catch(err => console.log(err));
+  // newInfo
+  //   .save()
+  //   .then(post => res.json(post))
+  //   .catch(err => console.log(err));
 });
+
+// router.route("getGitId/:id").get((req, res) => {
+//   GitInfo.findOne{
+
+//   }
+//     .then(info => res.json(info))
+//     .catch(err => console.log(err));
+
+//     console.log(req.params.id)
+
+// });
 
 module.exports = router;
