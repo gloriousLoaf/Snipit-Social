11b1c60b7de37a2a2e5137dbaf61030ae049ba21
gloriousLoaf <davidmetcalfwork@gmail.com>
2020-09-16 21:44:31 -0700

    Added timestamp to messages from other users.

--- client/src/components/chatrooms/Messages/Message/index.js
+++ client/src/components/chatrooms/Messages/Message/index.js
@@ -68,7 +68,8 @@ const Message = ({ message: { text, user }, name }) => {
                             </div>
                         </div>
                         <div className="uk-clearfix"></div>
-                        <p className="sentText paddingleft">{user}</p>
+                        <p className="sentText paddingLeft">{user}</p>
+                        <span className="sentText paddingLeft"> {new Date().toLocaleString()} </span>
                     </div>
                 </div>
             )
