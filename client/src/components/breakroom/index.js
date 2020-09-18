7acd31efb879132c7bfb1fc290a33604e0e681dc
mauricechouam <lansichouamou@gmail.com>
2020-09-18 16:04:27 -0700

    breakroom (design and functionality )

--- client/src/components/breakroom/index.js
+++ client/src/components/breakroom/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-no-target-blank */
 import React from "react";
 import NavBar from '../NavBar';
+import './style.css';
 
 //import stock
 import stock from "../img/portfolio-1.jpg";
@@ -18,17 +19,17 @@ class Portfolio extends React.Component {
           <div className="row">
             <div className="col-12">
               <div className="title-box text-center">
-                <h3 className="title-a">Games</h3>
+                <h3 className="title-a"></h3>
                 <div className="line-mf"></div>
               </div>
             </div>
           </div>
-          <div className="row  container" >
+          <div className="row  container " >
             <div className="col-lg-4 box">
-              <div className="work-box">
+              <div className="work-box polaroid">
                 <a href="https://playcanv.as/p/JtL2iqIH/" title="Play" target="_blank" >
                   <div className="work-img ">
-                    <img src={stock} alt="Demo live" className="img-fluid" />
+                    <img src={stock} alt="Demo live"  />
                   </div>
                 </a>
                 <div className="work-content ">
@@ -44,11 +45,12 @@ class Portfolio extends React.Component {
                 </div>
               </div>
             </div>
-            <div className="col-md-4">
+            
+            <div className="col-md-4 ">
               <div className="work-box"> 
                 <a href="https://emoji-runner.herokuapp.com/" title="Live Demo" target="_blank" >
-                  <div className="work-img">
-                    <img src={stock7} alt="Demo live" className="img-fluid" />
+                  <div className="work-img polaroid ">
+                    <img src={stock7} alt="Demo live" />
                   </div>
                 </a>
                 <div className="work-content">
@@ -69,7 +71,7 @@ class Portfolio extends React.Component {
               <div className="work-box">
                 <a href="https://dddance.party/?ref=three"  target="_blank" >
                   <div className="work-img ">
-                    <img src={stock6} alt="Demo live" className="img-fluid" />
+                    <img src={stock6} alt="Demo live"  />
                   </div>
                 </a>
                 <div className="work-content">
@@ -91,7 +93,7 @@ class Portfolio extends React.Component {
               <div className="work-box">
                 <a href="http://lab.sehsucht.de/"  target="_blank" >
                   <div className="work-img ">
-                    <img src={stock2} alt="Demo live" className="img-fluid" />
+                    <img src={stock2} alt="Demo live"  />
                   </div>
                 </a>
                 <div className="work-content">
@@ -117,9 +119,9 @@ TBD ..
 
             <div className="col-md-4 ">
               <div className="work-box">
-                <a href="https://maurice-employee-directory.herokuapp.com/" title="Live Demo" target="_blank" >
+                <a href="https://www.heroforge.com/"target="_blank" >
                   <div className="work-img ">
-                    <img src={stock3} alt="Demo live" className="img-fluid" />
+                    <img src={stock3} alt="Demo live"  />
                   </div>
                 </a>
                 <div className="work-content">
@@ -127,13 +129,9 @@ TBD ..
                     <div className="col-lg-12">
                       <div className="row">
                         <div className="col-12">
-                          <h2 className="w-title">Employee-Directory</h2>
+                          <h2 className="w-title">heroforge</h2>
                         </div>
-                        <a href="https://github.com/mauricechouam/React-Employee-Directory" title="Repository" target="_blank">
-                          <div className="col-12 w-ctegory w-more">
-                            <h6 className="ion-ios-plus-outline w-ctegory"> Github Repository</h6>
-                          </div>
-                        </a>
+                        
                       </div>
                       <div >
                         <p className="subtitle-a ">
@@ -148,9 +146,9 @@ TBD ..
 
             <div className="col-md-4 ">
               <div className="work-box">
-                <a href="https://maurice-burger.herokuapp.com/" title="Live Demo" target="_blank" >
+                <a href="https://phaserquest.herokuapp.com/" title="Live Demo" target="_blank" >
                   <div className="work-img ">
-                    <img src={stock5} alt="Demo live" className="img-fluid" />
+                    <img src={stock5} alt="Demo live"  />
                   </div>
                 </a>
                 <div className="work-content">
@@ -158,13 +156,9 @@ TBD ..
                     <div className="col-lg-12">
                       <div className="row">
                         <div className="col-12">
-                          <h2 className="w-title">Burger Restaurant </h2>
+                          <h2 className="w-title">Phaser Quest </h2>
                         </div>
-                        <a href="https://github.com/mauricechouam/Burger" title="Repository" target="_blank">
-                          <div className="col-12 w-ctegory w-more">
-                            <h6 className="ion-ios-plus-outline w-ctegory"> Github Repository</h6>
-                          </div>
-                        </a>
+                        
                       </div>
                       <div >
                         <p className="subtitle-a ">
